667c5383b14b1d9c48948a65dac84ff7424f0f27
Amin Nikbakht <amin_me@live.com>
2024-11-09 22:54:32 +0330

    up

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import PostView
+from .views import PostView, PostListView, CommentView, LikeView
 
 urlpatterns = [
     path('post/', PostView.as_view(), name='post'),
     path('post/<int:post_pk>/', PostView.as_view(), name='post'),
     path('post-list/', PostView.as_view(), name='post-list'),
+    path('post/<int:post_pk>/comment/', CommentView.as_view(), name='comment'),
+    path('post/<int:post_pk>/like/', LikeView.as_view(), name='like'),
 ]
